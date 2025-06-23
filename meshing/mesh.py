bb68283757d4df5376ff905689a5de589dc4d64b
TCSexy <kahaiant@gmail.com>
2025-06-23 19:38:26 -0400

    Changed file setup for package implementation

--- meshing/mesh.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Beam mesh takes in the length, width, and number of elements, and returns
-# two nparrays on x and y locations which correspond to node points
-def beamMesh(l, w, xnodes, ynodes):
-    xpoints = np.linspace(0, l, xnodes)
-    ypoints = np.linspace(0, w, ynodes)
-    print(xnodes)
-    
